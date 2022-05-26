bee5fa38f48c234b2f4384ae8860cfa907d1dbfc
Rokas Kasperavicius <goodname258@gmail.com>
2022-05-27 00:00:20 +0200

    first commit

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <main className={styles.main}>
+      <div className="flex justify-around pt-8">Welcome to the about.tsx</div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <main className={styles.main}>
+      <div className="flex justify-around pt-8">
+        <div className="flex justify-center text-2xl flex-col">
+          <div>I am Rokas,</div>
+          <div className="text-[#ff5208]">Student front-end developer</div>
+        </div>
+        <div className="relative w-[401px] h-[500px] rounded-3xl border-black overflow-hidden">
+          <Image
+            src="/hero.jpg"
+            alt="Vercel Logo"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
