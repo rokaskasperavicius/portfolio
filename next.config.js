ed1478d1350d34fd3b2f58febf9289bcc2f73626
Rokas Kasperavicius <goodname258@gmail.com>
2023-01-15 15:56:35 +0100

    locale fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+}
 
-  i18n: {
-    // providing the locales supported by your application
-    locales: ["en", "lt"],
-    //  default locale used when the non-locale paths are visited
-    defaultLocale: "en",
-  },
-};
-module.exports = nextConfig;
+module.exports = nextConfig
